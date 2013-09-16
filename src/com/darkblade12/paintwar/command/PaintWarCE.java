5070b575c60ad79c91305f79cce7ff530de8ef8d
DarkBlade12 <stefan.xD@gmx.de>
2013-09-16 19:54:28 +0200

    Removed something from the "/pw join <arena>" command which was only for testing purposes

--- src/com/darkblade12/paintwar/command/PaintWarCE.java
+++ src/com/darkblade12/paintwar/command/PaintWarCE.java
@@ -241,10 +241,10 @@ public class PaintWarCE implements CommandExecutor {
 			} else if (a.getState() != State.JOINABLE) {
 				sender.sendMessage(plugin.message.arena_game_x_started(true));
 				return true;
-			}/* else if (a.getPlayers().size() < 2) {
+			} else if (a.getPlayers().size() < 2) {
 				sender.sendMessage(plugin.message.arena_can_not_start());
 				return true;
-			}*/
+			}
 			if (p != null) {
 				PlayerStartCountdownEvent e = new PlayerStartCountdownEvent(p, a);
 				e.call();
